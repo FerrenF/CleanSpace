29815b5089ed87dc887e1e19cbb3c039ce06d388
Hunter W. <kfateweaver@gmail.com>
2025-09-18 01:40:56 -0400

    Update CleanSpaceClientPlugin.cs

--- ClientPlugin/CleanSpaceClientPlugin.cs
+++ ClientPlugin/CleanSpaceClientPlugin.cs
@@ -28,44 +28,7 @@ namespace CleanSpaceClient
 {
     internal static class ClientSessionParameterProviders
     {
-        public static void RegisterProviders()
-        {
-
-            ChatterChallengeFactory.RegisterProvider(RequestType.MethodIL, (args) => {
-
-                MethodIdentifier m = ProtoUtil.Deserialize<MethodIdentifier>((byte[])args[0]);
-                Type t = AppDomain.CurrentDomain.GetAssemblies().Select(a => a.GetType(m.FullName, throwOnError: false)).FirstOrDefault(x => x != null);
-
-                if (t == null)
-                    throw new InvalidOperationException($"Type {m.FullName} not found in loaded assemblies");
-
-                MethodBase method = t.GetMethod(m.MethodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
-
-                if (method == null)
-                    throw new InvalidOperationException($"Method {m.MethodName} not found on type {m.FullName}");
-
-                return ILAttester.GetMethodIlBytes(method);
-            });
-
-            /*     SessionParameterFactory.RegisterProvider(RequestType.TypeIL, (args) =>
-                 {
-                     MethodIdentifier m = ProtoUtil.Deserialize<MethodIdentifier>((byte[])args[0]);
-
-                     Type t = AppDomain.CurrentDomain
-                         .GetAssemblies()
-                         .Select(a => a.GetType(m.FullName, throwOnError: false))
-                         .FirstOrDefault(x => x != null);
-
-                     if (t == null)
-                         throw new InvalidOperationException($"Type {m.FullName} not found in loaded assemblies");
-
-                     return ILAttester.GetTypeIlBytes(t);
-                 });*/
-
-            Common.Logger.Info($"{Common.Logger}: Validation providers registered.");
-        }
-    }
-
+      
     // ReSharper disable once UnusedType.Global
     public class CleanSpaceClientPlugin : IPlugin, ICommonPlugin
     {
@@ -103,6 +66,40 @@ namespace CleanSpaceClient
                 Common.Logger.Error(m);
                 throw new InvalidOperationException(m);
             }
+
+            ChatterChallengeFactory.RegisterProvider(RequestType.MethodIL, (args) => {
+
+                MethodIdentifier m = ProtoUtil.Deserialize<MethodIdentifier>((byte[])args[0]);
+                Type t = m.InExecutingAssembly ? typeof(CleanSpaceClientPlugin)
+                : AppDomain.CurrentDomain.GetAssemblies().Select(a => a.GetType(m.FullName, throwOnError: false)).FirstOrDefault(x => x != null);
+
+                if (t == null)
+                    throw new InvalidOperationException($"Type {m.FullName} not found in loaded assemblies");
+
+                MethodBase method = t.GetMethod(m.MethodName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+                if (method == null)
+                    throw new InvalidOperationException($"Method {m.MethodName} not found on type {m.FullName}");
+
+                return ILAttester.GetMethodIlBytes(method);
+            });
+
+            /*     SessionParameterFactory.RegisterProvider(RequestType.TypeIL, (args) =>
+                 {
+                     MethodIdentifier m = ProtoUtil.Deserialize<MethodIdentifier>((byte[])args[0]);
+
+                     Type t = AppDomain.CurrentDomain
+                         .GetAssemblies()
+                         .Select(a => a.GetType(m.FullName, throwOnError: false))
+                         .FirstOrDefault(x => x != null);
+
+                     if (t == null)
+                         throw new InvalidOperationException($"Type {m.FullName} not found in loaded assemblies");
+
+                     return ILAttester.GetTypeIlBytes(t);
+                 });*/
+
+            Common.Logger.Info($"{Common.Logger}: Validation providers registered.");
         }
 
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
